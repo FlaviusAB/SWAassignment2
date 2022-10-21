97c45603af6d4c9b6bbb39559b962a7ba5c547cc
Faustas V <55922917+prOcenTas@users.noreply.github.com>
2022-10-21 14:22:28 +0200

    valid moves test passed

--- oo/src/helper.ts
+++ oo/src/helper.ts
@@ -6,15 +6,19 @@ export function matchLR(first: Position, second: Position, width: number, height
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
         let secondV = board[second.row][second.col];
-
-        if (second.col + 1 < width && second.col + 1 >= 0 && second.col - 1 < width && second.col - 1 >= 0) {
-            if (board[second.row][second.col + 1] === firstV && board[second.row][second.col - 1] === firstV) {
-                return true;
+        console.log("matchLR METHOD")
+        console.log("first row: " + first.row + " first col: " + first.col + " value: " + firstV)
+        console.log("second row: " + second.row + " second col: " + second.col + " value: " + secondV)
+        if (diagonalMove(first, second)) {
+            if (second.col + 1 < width && second.col + 1 >= 0 && second.col - 1 < width && second.col - 1 >= 0) {
+                if (board[second.row][second.col + 1] === firstV && board[second.row][second.col - 1] === firstV) {
+                    return true;
+                }
             }
-        }
-        else if (first.col + 1 < width && first.col + 1 >= 0 && first.col - 1 < width && first.col - 1 >= 0) {
-            if (board[first.row][first.col + 1] === secondV && board[first.row][first.col - 1] === secondV) {
-                return true;
+            if (first.col + 1 < width && first.col + 1 >= 0 && first.col - 1 < width && first.col - 1 >= 0) {
+                if (board[first.row][first.col + 1] === secondV && board[first.row][first.col - 1] === secondV) {
+                    return true;
+                }
             }
         }
         return false;
@@ -27,15 +31,19 @@ export function matchUD(first: Position, second: Position, height: number, width
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
         let secondV = board[second.row][second.col];
-
-        if (second.row + 1 < height && second.row + 1 >= 0 && second.row - 1 < height && second.row - 1 >= 0) {
-            if (board[second.row][second.col + 1] === firstV && board[second.row][second.col - 1] === firstV) {
-                return true;
+        console.log("matchUD METHOD")
+        console.log("first row: " + first.row + " first col: " + first.col + " value: " + firstV)
+        console.log("second row: " + second.row + " second col: " + second.col + " value: " + secondV)
+        if (diagonalMove(first, second)) {
+            if (second.row + 1 < height && second.row + 1 >= 0 && second.row - 1 < height && second.row - 1 >= 0) {
+                if (board[second.row][second.col + 1] === firstV && board[second.row][second.col - 1] === firstV) {
+                    return true;
+                }
             }
-        }
-        else if (first.col + 1 < height && first.col + 1 >= 0 && first.col - 1 < height && first.col - 1 >= 0) {
-            if (board[first.row + 1][first.col] === secondV && board[first.row - 1][first.col] === secondV) {
-                return true;
+            if (first.col + 1 < height && first.col + 1 >= 0 && first.col - 1 < height && first.col - 1 >= 0) {
+                if (board[first.row + 1][first.col] === secondV && board[first.row - 1][first.col] === secondV) {
+                    return true;
+                }
             }
         }
         return false;
@@ -48,15 +56,19 @@ export function match2D(first: Position, second: Position, height: number, width
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
         let secondV = board[second.row][second.col];
-
-        if (second.row + 2 < height && second.row + 2 >= 0 && second.row - 2 < height && second.row - 2 >= 0) {
-            if (board[second.row + 1][second.col] === firstV && board[second.row + 2][second.col] === firstV) {
-                return true;
+        console.log("match2D METHOD")
+        console.log("first row: " + first.row + " first col: " + first.col + " value: " + firstV)
+        console.log("second row: " + second.row + " second col: " + second.col + " value: " + secondV)
+        if (diagonalMove(first, second)) {
+            if (second.row + 2 < height && second.row + 2 >= 0) {
+                if (board[second.row + 1][second.col] === firstV && board[second.row + 2][second.col] === firstV) {
+                    return true;
+                }
             }
-        }
-        else if (first.row + 2 < height && first.row + 2 >= 0 && first.row - 2 < height && first.row - 2 >= 0) {
-            if (board[first.row + 1][first.col] === secondV && board[first.row + 2][first.col] === secondV) {
-                return true;
+            if (first.row + 2 < height && first.row + 2 >= 0) {
+                if (board[first.row + 1][first.col] === secondV && board[first.row + 2][first.col] === secondV) {
+                    return true;
+                }
             }
         }
         return false;
@@ -68,15 +80,19 @@ export function match2U(first: Position, second: Position, height: number, width
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
         let secondV = board[second.row][second.col];
-
-        if (second.row + 2 < height && second.row + 2 >= 0 && second.row - 2 < height && second.row - 2 >= 0) {
-            if (board[second.row - 1][second.col] === firstV && board[second.row - 2][second.col] === firstV) {
-                return true;
+        console.log("match2U METHOD")
+        console.log("first row: " + first.row + " first col: " + first.col + " value: " + firstV)
+        console.log("second row: " + second.row + " second col: " + second.col + " value: " + secondV)
+        if (diagonalMove(first, second)) {
+            if (second.row - 2 < height && second.row - 2 >= 0) {
+                if (board[second.row - 1][second.col] === firstV && board[second.row - 2][second.col] === firstV) {
+                    return true;
+                }
             }
-        }
-        else if (first.row + 2 < height && first.row + 2 >= 0 && first.row - 2 < height && first.row - 2 >= 0) {
-            if (board[first.row - 1][first.col] === secondV && board[first.row - 2][first.col] === secondV) {
-                return true;
+            if (first.row - 2 < height && first.row - 2 >= 0) {
+                if (board[first.row - 1][first.col] === secondV && board[first.row - 2][first.col] === secondV) {
+                    return true;
+                }
             }
         }
         return false;
@@ -88,15 +104,19 @@ export function match2R(first: Position, second: Position, width: number, height
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
         let secondV = board[second.row][second.col];
-
-        if (second.col + 2 < width && second.col + 2 >= 0 && second.col - 2 < width && second.col - 2 >= 0) {
-            if (board[second.row][second.col + 1] === firstV && board[second.row][second.col + 2] === firstV) {
-                return true;
+        console.log("match2R METHOD")
+        console.log("first row: " + first.row + " first col: " + first.col + " value: " + firstV)
+        console.log("second row: " + second.row + " second col: " + second.col + " value: " + secondV)
+        if (diagonalMove(first, second)) {
+            if (second.col + 2 < width && second.col + 2 >= 0) {
+                if (board[second.row][second.col + 1] === firstV && board[second.row][second.col + 2] === firstV) {
+                    return true;
+                }
             }
-        }
-        else if (first.col + 2 < width && first.col + 2 >= 0 && first.col - 2 < width && first.col - 2 >= 0) {
-            if (board[first.row][first.col + 1] === secondV && board[first.row][first.col + 2] === secondV) {
-                return true;
+            if (first.col + 2 < width && first.col + 2 >= 0) {
+                if (board[first.row][first.col + 1] === secondV && board[first.row][first.col + 2] === secondV) {
+                    return true;
+                }
             }
         }
         return false;
@@ -108,17 +128,28 @@ export function match2L(first: Position, second: Position, width: number, height
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
         let secondV = board[second.row][second.col];
-
-        if (second.col - 2 < width && second.col - 2 >= 0 && second.col + 2 < width && second.col + 2 >= 0) {
-            if (board[second.row][second.col - 1] === firstV && board[second.row][second.col - 2] === firstV) {
-                return true;
+        console.log("match2L METHOD")
+        console.log("first row: " + first.row + " first col: " + first.col + " value: " + firstV)
+        console.log("second row: " + second.row + " second col: " + second.col + " value: " + secondV)
+        if (diagonalMove(first, second)) {
+            if (second.col - 2 < width && second.col - 2 >= 0) {
+                if (board[second.row][second.col - 1] === firstV && board[second.row][second.col - 2] === firstV) {
+                    return true;
+                }
             }
-        }
-        else if (first.col - 2 < width && first.col - 2 >= 0 && first.col + 2 < width && first.col + 2 >= 0) {
-            if (board[first.row][first.col - 1] === secondV && board[first.row][first.col - 2] === secondV) {
-                return true;
+            if (first.col - 2 < width && first.col - 2 >= 0) {
+                if (board[first.row][first.col - 1] === secondV && board[first.row][first.col - 2] === secondV) {
+                    return true;
+                }
             }
         }
         return false;
     }
+}
+
+export function diagonalMove(first: Position, second: Position) {
+    if (first.row === second.row || first.col === second.col) {
+        return true;
+    }
+    return false;
 }
