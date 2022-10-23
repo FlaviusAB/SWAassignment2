4e47cc26924e96a477c7d89e7f93e45a3e874268
FlaviusAB <31958193+FlaviusAB@users.noreply.github.com>
2022-10-23 17:22:56 +0200

    extra (19 still)

--- oo/src/helper.ts
+++ oo/src/helper.ts
@@ -1,8 +1,8 @@
 import { Position , Match} from "./board";
 
-const matchThree= {} as Match<any>;
-export function matchLR(first: Position, second: Position, width: number, height: number, board: any): boolean {
 
+export function matchLR(first: Position, second: Position, width: number, height: number, board: any): Match<any> | undefined {
+    const matchThree= {} as Match<any>;
     if (first.row < height && first.row >= 0 && first.col < width && first.col >= 0 &&
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
@@ -10,12 +10,7 @@ export function matchLR(first: Position, second: Position, width: number, height
         if (diagonalMove(first, second)) {
             if (second.col + 1 < width && second.col + 1 >= 0 && second.col - 1 < width && second.col - 1 >= 0) {
                 if (board[second.row][second.col + 1] === firstV && board[second.row][second.col - 1] === firstV) {
-                    matchThree.matched = firstV;
-                    matchThree.positions?.push({row:second.row,col:second.col-1});
-                    matchThree.positions?.push({row:second.row,col:second.col});
-                    matchThree.positions?.push({row:second.row,col:second.col+1});
-                    console.log(matchThree);
-                    return true;
+                    return matchThree;
                 }
             }
             if (first.col + 1 < width && first.col + 1 >= 0 && first.col - 1 < width && first.col - 1 >= 0) {
@@ -24,16 +19,17 @@ export function matchLR(first: Position, second: Position, width: number, height
                     matchThree.positions?.push({row:first.row,col:first.col-1});
                     matchThree.positions?.push({row:first.row,col:first.col});
                     matchThree.positions?.push({row:first.row,col:first.col+1});
-                    return true;
+                    return matchThree;
                 }
             }
         }
-        return false;
+        return undefined;
     }
 
 }
 
-export function matchUD(first: Position, second: Position, height: number, width: number, board: any): boolean {
+export function matchUD(first: Position, second: Position, height: number, width: number, board: any): Match<any> | undefined {
+    const matchThree= {} as Match<any>;
     if (first.row < height && first.row >= 0 && first.col < width && first.col >= 0 &&
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
@@ -45,25 +41,27 @@ export function matchUD(first: Position, second: Position, height: number, width
                     matchThree.positions?.push({row:second.row-1,col:second.col});
                     matchThree.positions?.push({row:second.row,col:second.col});
                     matchThree.positions?.push({row:second.row+1,col:second.col});
-                    return true;
+                    return matchThree;
                 }
-            }
+            
             if (first.col + 1 < height && first.col + 1 >= 0 && first.col - 1 < height && first.col - 1 >= 0) {
                 if (board[first.row + 1][first.col] === secondV && board[first.row - 1][first.col] === secondV) {
                     matchThree.matched = secondV;
                     matchThree.positions?.push({row:first.row-1,col:first.col});
                     matchThree.positions?.push({row:first.row,col:first.col});
                     matchThree.positions?.push({row:first.row+1,col:first.col});
-                    return true;
+                    return matchThree;
                 }
             }
         }
-        return false;
+        }
+        return undefined;
     }
 
 }
 
-export function match2D(first: Position, second: Position, height: number, width: number, board: any): boolean {
+export function match2D(first: Position, second: Position, height: number, width: number, board: any): Match<any> | undefined {
+    const matchThree= {} as Match<any>;
     if (first.row < height && first.row >= 0 && first.col < width && first.col >= 0 &&
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
@@ -75,24 +73,26 @@ export function match2D(first: Position, second: Position, height: number, width
                     matchThree.positions?.push({row:second.row,col:second.col});
                     matchThree.positions?.push({row:second.row+1,col:second.col});
                     matchThree.positions?.push({row:second.row+2,col:second.col});
-                    return true;
+                    return matchThree;
                 }
-            }
+            
             if (first.row + 2 < height && first.row + 2 >= 0) {
                 if (board[first.row + 1][first.col] === secondV && board[first.row + 2][first.col] === secondV) {
                     matchThree.matched = secondV;
                     matchThree.positions?.push({row:first.row,col:first.col});
                     matchThree.positions?.push({row:first.row+1,col:first.col});
                     matchThree.positions?.push({row:first.row+2,col:first.col});
-                    return true;
+                    return matchThree;
                 }
             }
         }
-        return false;
+        }
+        return undefined;
     }
 }
 
-export function match2U(first: Position, second: Position, height: number, width: number, board: any): boolean {
+export function match2U(first: Position, second: Position, height: number, width: number, board: any): Match<any> | undefined {
+    const matchThree= {} as Match<any>;
     if (first.row < height && first.row >= 0 && first.col < width && first.col >= 0 &&
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
@@ -104,24 +104,26 @@ export function match2U(first: Position, second: Position, height: number, width
                     matchThree.positions?.push({row:second.row-2,col:second.col});
                     matchThree.positions?.push({row:second.row-1,col:second.col});
                     matchThree.positions?.push({row:second.row,col:second.col});
-                    return true;
+                    return matchThree;
                 }
-            }
+            
             if (first.row - 2 < height && first.row - 2 >= 0) {
                 if (board[first.row - 1][first.col] === secondV && board[first.row - 2][first.col] === secondV) {
                     matchThree.matched = secondV;
                     matchThree.positions?.push({row:first.row-2,col:first.col});
                     matchThree.positions?.push({row:first.row-1,col:first.col});
                     matchThree.positions?.push({row:first.row,col:first.col});
-                    return true;
+                    return matchThree;
                 }
             }
         }
-        return false;
+        }
+        return undefined;
     }
 }
 
-export function match2R(first: Position, second: Position, width: number, height: number, board: any): boolean {
+export function match2R(first: Position, second: Position, width: number, height: number, board: any): Match<any> | undefined{
+    const matchThree= {} as Match<any>;
     if (first.row < height && first.row >= 0 && first.col < width && first.col >= 0 &&
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
@@ -133,24 +135,23 @@ export function match2R(first: Position, second: Position, width: number, height
                     matchThree.positions?.push({row:second.row,col:second.col});
                     matchThree.positions?.push({row:second.row,col:second.col+1});
                     matchThree.positions?.push({row:second.row,col:second.col+2});
-                    return true;
+                    return matchThree;
                 }
-            }
+            
             if (first.col + 2 < width && first.col + 2 >= 0) {
                 if (board[first.row][first.col + 1] === secondV && board[first.row][first.col + 2] === secondV) {
-                    matchThree.matched = secondV;
-                    matchThree.positions?.push({row:first.row,col:first.col});
-                    matchThree.positions?.push({row:first.row,col:first.col+1});
-                    matchThree.positions?.push({row:first.row,col:first.col+2});
-                    return true;
+                
+                    return matchThree;
                 }
             }
         }
-        return false;
+        }
+        return undefined;
     }
 }
 
-export function match2L(first: Position, second: Position, width: number, height: number, board: any): boolean {
+export function match2L(first: Position, second: Position, width: number, height: number, board: any): Match<any> | undefined {
+    const matchThree= {} as Match<any>;
     if (first.row < height && first.row >= 0 && first.col < width && first.col >= 0 &&
         second.row < height && second.row >= 0 && second.col < width && second.col >= 0) {
         let firstV = board[first.row][first.col];
@@ -162,20 +163,21 @@ export function match2L(first: Position, second: Position, width: number, height
                     matchThree.positions?.push({row:second.row,col:second.col-2});
                     matchThree.positions?.push({row:second.row,col:second.col-1});
                     matchThree.positions?.push({row:second.row,col:second.col});
-                    return true;
+                    return matchThree;
                 }
-            }
+            
             if (first.col - 2 < width && first.col - 2 >= 0) {
                 if (board[first.row][first.col - 1] === secondV && board[first.row][first.col - 2] === secondV) {
                     matchThree.matched = secondV;
                     matchThree.positions?.push({row:first.row,col:first.col-2});
                     matchThree.positions?.push({row:first.row,col:first.col-1});
                     matchThree.positions?.push({row:first.row,col:first.col});
-                    return true;
+                    return matchThree;
                 }
             }
         }
-        return false;
+        }
+        return undefined;
     }
 }
 
@@ -185,3 +187,45 @@ export function diagonalMove(first: Position, second: Position) {
     }
     return false;
 }
+
+// export function markForCrush<T>(boardValues: any[], width: number, height: number): Position[] {
+//     const arr: Match<T>[] = [];
+
+//     //check per width
+//     let widthAccumulator: Match<T>[] = [];
+//     for (let i = 0; i < height; i++) {
+//         for (let j = 0; j < width; j++) {
+//             if (widthAccumulator.length === 0 || (j > 0 && widthAccumulator[widthAccumulator.length - 1].matched === boardValues[i][j - 1])) {
+//                 widthAccumulator.push({matched: boardValues[i][j - 1], positions:[{col: i, row: j}]})
+//                 console.log("col"+ i + " row:"+j)
+//             } else {
+//                 if (widthAccumulator.length < 3) {
+//                     widthAccumulator = [];
+//                 } else {
+//                     arr.push(...widthAccumulator);
+//                 }
+//                 widthAccumulator[i].positions.push({ col: i, row: j} )
+//             }
+//         }
+//     }
+
+//     //check per width
+//     let heightAccumulator: Position[] = [];
+//     for (let i = 0; i < width; i++) {
+//         for (let j = 0; j < height; j++) {
+//             if (heightAccumulator.length === 0 || (j > 0 && heightAccumulator[heightAccumulator.length - 1] === boardValues[i][j - 1])) {
+//                 heightAccumulator.push({ col: i, row: j } as Position)
+//                 console.log("col"+ i + " row:"+j)
+//             } else {
+//                 if (heightAccumulator.length < 3) {
+//                     heightAccumulator = [];
+//                 } else {
+//                     arr.push(...heightAccumulator);
+//                 }
+//                 heightAccumulator.push({ col: i, row: j } as Position)
+//             }
+//         }
+//     }
+
+//     return arr;
+// }
