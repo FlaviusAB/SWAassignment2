af09290ed416b1e40d832f9687d8465ed5959e7d
FlaviusAB <31958193+FlaviusAB@users.noreply.github.com>
2022-12-19 12:08:26 +0100

    func created

--- functional/src/board.ts
+++ functional/src/board.ts
@@ -1,3 +1,4 @@
+import { findMatchesWhenMoving, findMatchesRowAndColumn, replaceTiles} from "./helper"
 export type Generator<T>= { next:() => T } 
 
 export type Position = {
@@ -10,9 +11,20 @@ export type Match<T> = {
     positions: Position[]
 }    
 
-export type Board<T> = ?;
+export type Board<T> = {
+    width: number,
+    height: number,
+    boardValues: any[]
+};
 
-export type Effect<T> = ?;
+export type Effect<T> = {
+    kind: 'Match',
+    match: Match<T>
+} | 
+{
+    kind: 'Refill',
+    board: Board<T>
+}
 
 export type MoveResult<T> = {
     board: Board<T>,
@@ -20,13 +32,82 @@ export type MoveResult<T> = {
 }    
 
 export function create<T>(generator: Generator<T>, width: number, height: number): Board<T> {
+    
+    let boardValues = []
+    for (let i = 0; i < height; i++) {
+        let row = []
+        for (let j = 0; j < width; j++) {
+            row.push(generator.next())
+        }
+        boardValues.push(row)
+    }
+    return {width: width, height: height, boardValues: boardValues}
 }    
 
 export function piece<T>(board: Board<T>, p: Position): T | undefined {
+    if (board.boardValues[p.row] === undefined)
+            return undefined
+
+        return board.boardValues[p.row][p.col]
 }    
 
 export function canMove<T>(board: Board<T>, first: Position, second: Position): boolean {
+    let matches:Match<T>[] = findMatchesWhenMoving(first, second, board.width, board.height, board.boardValues)       
+   
+        if(matches[0]===undefined)
+            return false
+        return true;
 }
 
 export function move<T>(generator: Generator<T>, board: Board<T>, first: Position, second: Position): MoveResult<T> {
-}
+        let effects:Effect<T>[] = []
+        let matches:Match<T>[] = []
+        matches = findMatchesWhenMoving(first, second, board.width, board.height, board.boardValues)
+        let positionsForReplacing :Position[] = []
+        let matchedPositionsAfterRefil :Position[] = []
+        let replacedBoard
+        let replacedBoardAfter
+
+
+        if(matches[0]!==undefined){
+            matches.forEach(matchedValues =>{
+               
+                matchedValues.positions.forEach(p=>{
+                        positionsForReplacing.push(p)
+                    })
+                    
+                    effects.push( {kind:'Match', match:matchedValues});
+                    
+                })
+                let firstValue = board.boardValues[first.row][first.col];
+                let secondValue = board.boardValues[second.row][second.col];
+                board.boardValues[first.row][first.col] = secondValue;
+                board.boardValues[second.row][second.col] = firstValue;     
+
+            replacedBoard = replaceTiles(positionsForReplacing,board.width,board.height,board.boardValues,generator)
+            effects.push({kind:'Refill', board:replacedBoard});
+            
+
+            let matchesAfterRefil = findMatchesRowAndColumn(board.width,board.height,replacedBoard)
+            while(matchesAfterRefil[0]!==undefined){
+                matchesAfterRefil.forEach(matchedValues =>{
+               
+                    matchedValues.positions.forEach(p=>{
+                        matchedPositionsAfterRefil.push(p)
+                        })
+                        
+                        effects.push({kind:'Match', match:matchedValues});
+                        
+                    })
+                    replacedBoardAfter = replaceTiles(matchedPositionsAfterRefil,board.width,board.height,board.boardValues,generator)
+                    effects.push({kind:'Refill', board:replacedBoardAfter});
+                    
+                    matchesAfterRefil = findMatchesRowAndColumn(board.width,board.height,replacedBoardAfter)
+            }
+             
+        }
+      
+      return {board: board, effects:effects}  
+        
+    }
+
---
+++ functional/src/helper.ts
@@ -0,0 +1,132 @@
+import {
+    Position,
+    Match,
+    Board
+} from "./board";
+export type Generator<T> = { next: () => T }
+
+export function isNotDiagonalMove(first: Position, second: Position) {
+    if (first.row === second.row || first.col === second.col) {
+        return true;
+    }
+    return false;
+}
+
+export function findMatchesWhenMoving(first: Position, second: Position, width: number, height: number, board: any): Match < any >[] | undefined {
+    
+    let allMatches: Match<any>[] = []
+
+    if (first.row < height && first.row >= 0 && first.col < width && first.col >= 0 &&
+        second.row < height && second.row >= 0 && second.col < width && second.col >= 0 && isNotDiagonalMove(first,second)) {
+        
+        let firstValue = board[first.row][first.col];
+        let secondValue = board[second.row][second.col];
+       
+        board[first.row][first.col] = secondValue;
+        board[second.row][second.col] = firstValue;
+        
+        allMatches = findMatchesRowAndColumn(width,height,board)
+        
+        board[first.row][first.col] = firstValue;
+        board[second.row][second.col] = secondValue;
+    }
+    return allMatches;
+}
+
+export function replaceTiles(positions:Position[],width: number, height: number, board: any,generator:Generator<any>) : Board<any>{
+    positions.forEach(p=>{
+        board[p.row][p.col]=undefined
+    })
+    
+    // console.log("h:"+height)
+    // console.log("w:"+width)
+    let inc = height-1  
+    while(inc>=0)
+    {
+    for (let i = height-1; i >= 0; i--) {
+        for (let j = 0; j < width; j++) {
+            if(board[i][j]===undefined)
+            {
+                if(i===0)
+                {
+                    board[i][j]=generator.next()
+                }
+                else{
+                    if(board[i-1][j]!==undefined)
+                    {
+                    board[i][j]=board[i-1][j]
+                    board[i-1][j]=undefined
+                    }
+                }
+            }
+            // console.log("["+i+","+j+"]")
+            // console.log(board[i][j])
+
+        }
+    }
+    inc--
+    
+}
+    
+    return board;
+}
+
+export function findMatchesRowAndColumn(width:number,height:number,board:any):Match < any >[] | undefined{
+    let matchesFound: Match<any>[] = []
+    
+     //check if 3 matches on every column
+     for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width - 2; j++) {
+
+            if (board[i][j] === board[i][j + 1] && board[i][j + 1] === board[i][j + 2]) {
+                let matchThree = {}as Match < any > ;
+                matchThree.positions = []
+                let positions: Position[] = [];
+                positions.push({
+                    row: i,
+                    col: j
+                })
+                positions.push({
+                    row: i,
+                    col: j + 1
+                })
+                positions.push({
+                    row: i,
+                    col: j + 2
+                })
+
+                matchThree.positions = Object.assign([], positions)
+                matchThree.matched = board[i][j]
+                matchesFound.push(matchThree)
+
+            }
+        }
+    }
+     //check if 3 matches on every row
+     for (let i = 0; i < height - 2; i++) {
+         for (let j = 0; j < width; j++) {
+             if (board[i][j] === board[i + 1][j] && board[i + 1][j] === board[i + 2][j]) {
+                 let matchThree1 = {}as Match < any > ;
+                 matchThree1.positions = []
+                 let positions1: Position[] = [];
+                 positions1.push({
+                     row: i,
+                     col: j
+                 })
+                 positions1.push({
+                     row: i + 1,
+                     col: j
+                 })
+                 positions1.push({
+                     row: i + 2,
+                     col: j
+                 })
+
+                 matchThree1.positions = Object.assign([], positions1)
+                 matchThree1.matched = board[i][j]
+                 matchesFound.push(matchThree1)
+             }
+         }
+     }
+     return matchesFound
+}
